65a3e9c70910fa6f8d2782519cd839a5fec57994
Francisco Crespo <fcrespo8@hotmail.com>
2022-11-30 08:57:29 +0100

    Chatroom

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "movies/:id/chatrooms/:id", to: "chat_rooms#show"
+  get "profile/:id", to: "pages#profile"
+
   resources :movies, only: %i[index show]
   resources :posts, only: %i[index show new create edit update] do
     resources :replies, only: %i[new create destroy]
