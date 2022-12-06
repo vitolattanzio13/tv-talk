220248729ded3f1521b5cce0fe7731d24e95c2f4
Silviu Rosu <silviurosu1988@gmail.com>
2022-12-06 11:05:23 +0100

    fixed followed_user

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,6 @@ class User < ApplicationRecord
   has_many :followed_movies
   has_one :notification
   has_one_attached :photo
+  has_many :followed_users_as_followee, class_name: "FollowedUser", foreign_key: :followee_id
+  has_many :followed_users_as_follower, class_name: "FollowedUser", foreign_key: :follower_id
 end
