73fc4aa53fb906c91db1dd6acda55682578e6628
Francisco Crespo <fcrespo8@hotmail.com>
2022-12-07 11:25:33 +0100

    profile fixed

--- app/javascript/controllers/profile_tabs_controller.js
+++ app/javascript/controllers/profile_tabs_controller.js
@@ -2,41 +2,66 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="profile-tabs"
 export default class extends Controller {
-  static targets = [ "followedMovies", "followedUsers", "posts", "buttonFollowedMovies", "buttonFollowedUsers", "buttonPosts" ]
+  static targets = [ "followedMovies", "followedUsers", "posts", "notifications", "buttonFollowedMovies", "buttonFollowedUsers", "buttonPosts", "buttonNotifications" ]
 
   connect() {
-    console.log("fuck youuuuuuuuuuuuuu")
     this.buttonFollowedMoviesTarget.classList.add("btn-profile-tab-active")
   }
 
   showFollowedMovies() {
-    console.log("fuck youuuuuuuuuuuuuu2")
+    if (this.buttonNotificationsTarget !== null) {
+      this.buttonNotificationsTarget.classList.remove("btn-profile-tab-active")
+    }
     this.buttonFollowedMoviesTarget.classList.add("btn-profile-tab-active")
     this.buttonFollowedUsersTarget.classList.remove("btn-profile-tab-active")
     this.buttonPostsTarget.classList.remove("btn-profile-tab-active")
+    this.buttonNotificationsTarget.classList.remove("btn-profile-tab-active")
     this.followedMoviesTarget.classList.remove("d-none")
     this.followedUsersTarget.classList.add("d-none")
     this.postsTarget.classList.add("d-none")
+    this.notificationsTarget.classList.add("d-none")
   }
 
   showFollowedUsers() {
-    console.log("fuck youuuuuuuuuuuuuu3")
+    if (this.buttonNotificationsTarget !== null) {
+      this.buttonNotificationsTarget.classList.remove("btn-profile-tab-active")
+    }
     this.buttonFollowedMoviesTarget.classList.remove("btn-profile-tab-active")
     this.buttonFollowedUsersTarget.classList.add("btn-profile-tab-active")
     this.buttonPostsTarget.classList.remove("btn-profile-tab-active")
+    this.buttonNotificationsTarget.classList.remove("btn-profile-tab-active")
     this.followedMoviesTarget.classList.add("d-none")
     this.followedUsersTarget.classList.remove("d-none")
     this.postsTarget.classList.add("d-none")
+    this.notificationsTarget.classList.add("d-none")
   }
 
   showPosts() {
-    console.log("fuck youuuuuuuuuuuuuu4")
+    if (this.buttonNotificationsTarget !== null) {
+      this.buttonNotificationsTarget.classList.remove("btn-profile-tab-active")
+    }
     this.buttonFollowedMoviesTarget.classList.remove("btn-profile-tab-active")
     this.buttonFollowedUsersTarget.classList.remove("btn-profile-tab-active")
     this.buttonPostsTarget.classList.add("btn-profile-tab-active")
+    this.buttonNotificationsTarget.classList.remove("btn-profile-tab-active")
     this.followedMoviesTarget.classList.add("d-none")
     this.followedUsersTarget.classList.add("d-none")
     this.postsTarget.classList.remove("d-none")
+    this.notificationsTarget.classList.add("d-none")
+  }
+
+  showNotifications() {
+    if (this.buttonNotificationsTarget !== null) {
+      this.buttonNotificationsTarget.classList.remove("btn-profile-tab-active")
+    }
+    this.buttonFollowedMoviesTarget.classList.remove("btn-profile-tab-active")
+    this.buttonFollowedUsersTarget.classList.remove("btn-profile-tab-active")
+    this.buttonPostsTarget.classList.remove("btn-profile-tab-active")
+    this.buttonNotificationsTarget.classList.add("btn-profile-tab-active")
+    this.followedMoviesTarget.classList.add("d-none")
+    this.followedUsersTarget.classList.add("d-none")
+    this.postsTarget.classList.add("d-none")
+    this.notificationsTarget.classList.remove("d-none")
   }
 
 }
