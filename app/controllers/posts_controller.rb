7eddcb2b488d88407fe1db7e4dbd6f59a28c1019
Silviu <silviurosu1988@gmail.com>
2023-02-18 13:24:05 +0100

    will authenticate before every page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[index]
+  # skip_before_action :authenticate_user!, only: %i[index]
 
   def index
     @posts = Post.all.order(created_at: :desc)
